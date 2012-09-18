0b68815d0960fc7016cc55e1db490a75bcba0d63
Franz Liedke <franz@develophp.org>
2012-09-18 15:35:08 +0200

    Extract base HTML into layout view.

---
+++ views/layout/main.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en" dir="ltr">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Install FluxBB</title>
+</head>
+
+<body>
+
+<div id="flux-install">
+
+@yield('main')
+
+</div>
+
+</body>
+</html>
--- views/start.blade.php
+++ views/start.blade.php
@@ -1,17 +1,9 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Install FluxBB</title>
-</head>
+@layout('fluxbb_installer::layout.main')
 
-<body>
-
+@section('main')
 	<h1>Install FluxBB</h1>
 	<p>Be sure to have setup your database connection information in the file <code>application/config/database.php</code>.</p>
 	<form action="{{ URL::to_action('fluxbb_installer::home@install') }}" method="post">
 		<input type="submit" value="Start!" />
 	</form>
-
-</body>
-</html>
+@endsection
