53141a77e861d2b5b73bd3f5d10fa088292ff19d
Franz Liedke <franz@develophp.org>
2012-09-21 01:15:56 +0200

    Move an installer submit button around. The Laravel profiler bar is annoying me.

--- views/run.blade.php
+++ views/run.blade.php
@@ -7,8 +7,8 @@
 	<form method="POST">
 		<p>You're ready to run the FluxBB installation.</p>
 		<p>TODO: Review information here.</p>
-		<pre><?php print_r(Session::get('fluxbb.install')); ?></pre><br>
 		<input type="submit" value="Weiter" />
+		<pre><?php print_r(Session::get('fluxbb.install')); ?></pre>
 	</form>
 
 @endsection
