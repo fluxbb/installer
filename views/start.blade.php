39d44ab9eeb0608e6b7b6a61281a8747bc8277e9
Franz Liedke <franz@develophp.org>
2012-12-21 14:11:25 +0100

    Move and update views.

--- views/start.blade.php
+++ views/start.blade.php
@@ -3,7 +3,7 @@
 @section('main')
 
 	<p>This installer is going to guide you through the process.</p>
-	<form action="{{ URL::route('installer_start') }}" method="POST">
+	<form method="POST">
 		<label for="language">Installation language</label>
 		<select name="language" id="language">
 			<option value="en">English</option>
