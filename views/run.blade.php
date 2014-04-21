12a48c7f654bf20b7efa4f52864e6fa39c36fd55
Franz Liedke <franz@develophp.org>
2014-04-21 19:52:23 +0200

    Whitespace cleanup.

--- views/run.blade.php
+++ views/run.blade.php
@@ -2,23 +2,21 @@
 
 @section('main')
 
-	<!-- begin installer panel -->
-	<div class="panel panel-info">
-		<div class="panel-heading">
-			<h3 class="panel-title">Run</h3>
-		</div>
-		<div class="panel-body">
-			<form method="post" role="form">
-				
-				<p>We have all the information we need. You are ready to run the FluxBB installation.</p>
-				<p>TODO: Review your information here.</p>
-				<input class="btn btn-primary" type="submit" value="Continue" />
-				
-			</form>
-		</div>
-	</div>
-	<!-- end installer panel -->
+    <!-- begin installer panel -->
+    <div class="panel panel-info">
+        <div class="panel-heading">
+            <h3 class="panel-title">Run</h3>
+        </div>
+        <div class="panel-body">
+            <form method="post" role="form">
 
-	
+                <p>We have all the information we need. You are ready to run the FluxBB installation.</p>
+                <p>TODO: Review your information here.</p>
+                <input class="btn btn-primary" type="submit" value="Continue" />
+
+            </form>
+        </div>
+    </div>
+    <!-- end installer panel -->
 
 @stop
