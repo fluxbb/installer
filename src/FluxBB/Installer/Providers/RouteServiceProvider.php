d55f98953b0f3e974c60952b5c7f37a252769742
Franz Liedke <franz@develophp.org>
2014-10-28 10:48:47 +0100

    Update RouteServiceProvider.

--- src/FluxBB/Installer/Providers/RouteServiceProvider.php
+++ src/FluxBB/Installer/Providers/RouteServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace FluxBB\Installer\Providers;
 
+use FluxBB\Web\Router;
+use Illuminate\Contracts\View\Factory;
 use Illuminate\Support\ServiceProvider;
 
 class RouteServiceProvider extends ServiceProvider
@@ -12,7 +14,34 @@ class RouteServiceProvider extends ServiceProvider
      * @return void
      */
     public function register()
+    {}
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
     {
-        include __DIR__.'/../../../../routes.php';
+        // Add a view namespace for our views
+        $this->app->extend('view', function (Factory $view) {
+            $view->addNamespace('fluxbb_installer', __DIR__.'/../../../views/');
+            return $view;
+        });
+
+        $router = $this->app->make('FluxBB\Web\Router');
+        $this->registerRoutes($router);
+    }
+
+    /**
+     * Register the installation routes.
+     *
+     * @param \FluxBB\Web\Router $router
+     * @return void
+     */
+    protected function registerRoutes(Router $router)
+    {
+        $router->get('/install', 'install', 'FluxBB\Installer\Web\Controller@run');
+        $router->post('/install', 'post_install', 'FluxBB\Installer\Web\Controller@run');
     }
 }
